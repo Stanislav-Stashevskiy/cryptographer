c04929034860c8d80d4c8ae5a73687cd745d0fdb
Stanislav-Stashevskiy <75270781+Stanislav-Stashevskiy@users.noreply.github.com>
2020-12-01 11:12:04 +0300

    Add files via upload

---
+++ cipher_cezara.py
@@ -0,0 +1,51 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+'x' ,'y', 'z', ' ']
+crypto_letters = []
+s = [' ', '!', '?', '(', ')']
+
+do = input('Зашифровать или разшифровать?(1/2):')
+if do == '1':
+    cod = input('Введите код RОТ(1, 2, 3, 4...25):')
+    cod = int(cod)
+    while True:
+        what = input('Введите слово:')
+
+        if what == 'stop':
+            break
+
+        for c in what:
+            if c in s:
+                crypto_letters.append(c)
+                continue
+            ind_c = letters.index(c)
+            crypto_index = ind_c + cod
+            if crypto_index > 25:
+                crypto_index = 25 - ind_c
+            letter = letters[crypto_index]
+            crypto_letters.append(letter)
+        print(crypto_letters)
+        crypto_letters.clear()
+    print('Программа завершина!!!')
+
+elif do == '2':
+    cod = input('Введите код RОТ(1, 2, 3, 4...25):')
+    cod = int(cod)
+    while True:
+        what = input('Введите слово:')
+
+        if what == 'stop':
+            break
+
+        for c in what:
+            if c in s:
+                crypto_letters.append(c)
+                continue
+            ind_c = letters.index(c)
+            crypto_index = ind_c - cod
+            if crypto_index > 25:
+                crypto_index = 25 + ind_c
+            letter = letters[crypto_index]
+            crypto_letters.append(letter)
+        print(crypto_letters)
+        crypto_letters.clear()
+    print('Программа завершина!!!')
